cf478584e360400e5bc7ae6624a8dab41af2c3e1
mytearshurt6 <mytearshurt6@gmail.com>
2026-08-21 04:04:32 +0400

    Added the feed layout UI

---
+++ src/modules/auth/ui/components/auth-button.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import { UserCircleIcon } from 'lucide-react'
+
+export function AuthButton() {
+  return (
+    <Button
+      variant="outline"
+      className="px-4 py-2 text-sm font-medium text-blue-600 hover:text-blue-500 border-blue-500/2 rounded-full shadow-none">
+      <UserCircleIcon />
+      Sign in
+    </Button>
+  )
+}
---
+++ src/modules/home/ui/components/home-sidebar/personal-section.tsx
@@ -0,0 +1,55 @@
+'use client'
+
+import {
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from '@/components/ui/sidebar'
+import { HistoryIcon, ListVideoIcon, ThumbsUp } from 'lucide-react'
+import Link from 'next/link'
+
+const items = [
+  {
+    title: 'History',
+    url: '/playlists/history',
+    icon: HistoryIcon,
+    auth: true,
+  },
+  {
+    title: 'Liked videos',
+    url: '/playlists/liked',
+    icon: ThumbsUp,
+    auth: true,
+  },
+  {
+    title: 'All playlists',
+    url: '/playlists',
+    icon: ListVideoIcon,
+    auth: true,
+  },
+]
+
+export function PersonalSection() {
+  return (
+    <SidebarGroup>
+      <SidebarGroupLabel>You</SidebarGroupLabel>
+      <SidebarGroupContent>
+        <SidebarMenu>
+          {items.map((item) => (
+            <SidebarMenuItem key={item.title}>
+              <SidebarMenuButton tooltip={item.title} asChild isActive={false} onClick={() => {}}>
+                <Link href={item.url} className="flex items-center gap-4">
+                  <item.icon />
+                  <span className="text-sm">{item.title}</span>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          ))}
+        </SidebarMenu>
+      </SidebarGroupContent>
+    </SidebarGroup>
+  )
+}
